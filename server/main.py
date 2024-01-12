dc89b4471842101b65ecec05052ddc3641fd749c
shreyash-01 <singhshreyash0075@gmail.com>
2024-01-12 07:26:48 +0530

    Commit

--- server/main.py
+++ server/main.py
@@ -1,21 +1,34 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
+import json
 
 import classify_script
 import database
 
 # PORT 3031
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/api/data/detect")
-def check_pattern(patterns_list: list[str]) -> bool:
+def check_pattern(patterns_list: dict):
     """
     Detects if the website is using dark pattern or not.
     :param patterns_list: list of text from the website
     :return: True or False depending on the categorization of the dark pattern
     """
-    return classify_script.predict_category(patterns_list)
+    s = classify_script.predict_category(set(patterns_list["text"].split("\n")))
+    return {"Res": list(s)}
 
 
 @app.post("/api/data/report")
@@ -25,5 +38,5 @@ def report_pattern(report_data: dict) -> JSONResponse:
     :param report_data:
     :return: JSON response 200 if the dark pattern is reported Successfully, else Error
     """
-    database.insert_report(report_data['email'], report_data['url'], report_data['pattern_name'], report_data['html_code'], report_data['user_comment'])
+    #database.insert_report(report_data['email'], report_data['url'], report_data['pattern_name'], report_data['html_code'], report_data['user_comment'])
     return JSONResponse(content="Reported Dark Pattern!! Thank You", status_code=200)
--- vite-extension/src/App.tsx
+++ vite-extension/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     const targetUrl = 'http://localhost:3000/form'; // Replace with your desired URL
     chrome.tabs.update({ url: targetUrl });
   };
-  
+
   const handleClick= async()=>{
     let [tab]=await chrome.tabs.query({active:true});
     chrome.scripting.executeScript({
@@ -20,9 +20,9 @@ function App() {
         document.execCommand('copy');
         document.body.removeChild(dummyTextArea);
 
-        alert('Text copied: ' + textToCopy);
+        console.log(textToCopy)
 
-        const apiUrl = 'https://your-server-api-endpoint.com';
+        const apiUrl = 'http://localhost:8000/api/data/detect';
         try {
           const response = await fetch(apiUrl, {
             method: 'POST',
@@ -34,13 +34,17 @@ function App() {
 
           if (response.ok) {
             const responseData = await response.json();
-            alert('Server Response:\n' + JSON.stringify(responseData, null, 2));
+            console.log(responseData["Res"])
+
+
+
+
           } else {
             alert('Error sending text to the server.');
           }
         } catch (error) {
           console.error('Error:', error);
-          alert('Error sending text to the server.');
+          alert('Catch');
         }
       }
     });
@@ -49,12 +53,12 @@ function App() {
 
   return (
     <div className='flex flex-col items-center w-[300px] bg-gray-800 h-[200px] border-[#989494] border-[2px]'>
-      
+
       <div className="flex justify-between mt-5 pb-2 border-b-[0.5px] border-[#b1adad] w-full">
-      
+
       <button type="button" onClick={handleRedirect} className="w-[130px] ml-7 h-[35px] text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Dark Pattern</button>
       <button type="button" className=" w-[80px] mr-4 h-[35px] text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-2 py-1 text-center me-2 mb-2">Feedback</button>
-       
+
       </div>
       <div className="mt-9 mb-3">
         <button onClick={handleClick} className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800">
